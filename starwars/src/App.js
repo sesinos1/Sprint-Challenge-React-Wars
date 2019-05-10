7bfb6f64a4a1464dfe089f3487937780962f081e
sesinos tesfamariam <sesinostesfamariam@sesinoss-mbp.attlocal.net>
2019-05-10 10:00:35 -0700

    finished-rendering-info

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import Characters from './components/Characters'
 
 class App extends Component {
   constructor() {
@@ -32,7 +33,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1 className="Header">React Wars</h1>
+        <h1 className="Header">React Wars Dating Profiles</h1>
+        <Characters starwarsChars={this.state.starwarsChars} />
       </div>
     );
   }
